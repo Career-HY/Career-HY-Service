ce0b0807cbf4eced1ee9b6441390be079c04c854
Luti <wns6619@gmail.com>
2025-05-23 05:26:51 +0900

    feat: User 스키마 정의

--- backend/src/schemas/user.py
+++ backend/src/schemas/user.py
@@ -1,11 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class UserCreate(BaseModel):
-    emaiL: str
-    password: str
+    email: str
+    pwd: str
 
 class UserRead(BaseModel):
-    id: int
+    id: str
     email: str
+    signup_date: Optional[datetime]
     class Config:
         orm_mode = True
