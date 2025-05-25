fc592822733652155ca13a32be1ee4d71221439b
Luti <wns6619@gmail.com>
2025-05-26 04:44:06 +0900

    refactor: 로그인 라우터 함수 수정

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from config.config import settings
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from db.session import get_db
 from api.routers import user
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
 app.include_router(user.router, prefix="/users", tags=["users"])
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 
 @app.get("/")
 def root():
