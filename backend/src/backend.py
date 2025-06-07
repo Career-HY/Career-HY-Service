8c4da69dd421788549a06ddd9cd743b5c382a156
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-06-08 08:28:51 +0900

    test: deploy-test (#91)

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -39,7 +39,7 @@ app.include_router(chat.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Career-Hi Backend!! 6/8 08:15 test"}
+    return {"message": "Welcome to Career-Hi Backend!! 6/8 08:30 test"}
 
 
 @app.get("/health")
