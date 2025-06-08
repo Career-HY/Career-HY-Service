56df50fd10263fbf412f27ffb139169d2c525bb4
Luti <wns6619@gmail.com>
2025-06-09 01:17:07 +0900

    feat: 홈 화면 안내문구

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/home/header'
 import HeroSection from '@/components/home/hero-section'
 import FeatureList from '@/components/home/feature-list'
 import PortalCard from '@/components/home/portal-card'
+import ServiceNotice from '@/components/home/service-notice'
 import Footer from '@/components/home/footer'
 
 export default function Home() {
@@ -11,22 +12,28 @@ export default function Home() {
       <Header />
 
       {/* 메인 컨텐츠 */}
-      <div className="flex-1 container mx-auto px-4 py-4">
-        <div className="flex flex-col lg:flex-row items-start justify-center gap-8 h-full">
+      <div className="flex-1 container mx-auto px-4 py-4 flex flex-col">
+        {/* 상단 섹션: 정보 + 로그인 */}
+        <div className="flex flex-col lg:flex-row items-start justify-center gap-8">
           {/* 좌측 정보 섹션 */}
           <div className="flex-1 max-w-lg">
             <div className="space-y-6">
               <HeroSection />
               <FeatureList />
-              <PortalCard />
+              <div className="pt-4">
+                <PortalCard />
+              </div>
             </div>
           </div>
 
-          {/* 우측 로그인 폼 */}
+          {/* 우측 섹션 */}
           <div className="flex-1 max-w-md w-full">
             <LoginForm />
           </div>
         </div>
+
+        {/* 안내 문구 섹션 */}
+        <ServiceNotice />
       </div>
 
       <Footer />
