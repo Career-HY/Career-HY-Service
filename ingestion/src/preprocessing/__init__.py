54baa070d1e88f1c693c7140716a3390e4d5398e
rettooo <gkdud9236@naver.com>
2025-12-04 09:54:01 +0900

    feat(ingestion): preprocessing 모듈에 JobPostParser export 추가
    - __init__.py에 JobPostParser import 및 export 추가
    - __all__에 JobPostParser 추가
    
    관련: Phase 1 (Experiment 전략 적용)

--- ingestion/src/preprocessing/__init__.py
+++ ingestion/src/preprocessing/__init__.py
@@ -1,7 +1,9 @@
 """
 텍스트 전처리 모듈
 """
+
 from .text_loader import extract_text_PyMuPDF
 from .text_cleaner import clean_text
+from .job_post_parser import JobPostParser
 
-__all__ = ["extract_text_PyMuPDF", "clean_text"] 
+__all__ = ["extract_text_PyMuPDF", "clean_text", "JobPostParser"]
